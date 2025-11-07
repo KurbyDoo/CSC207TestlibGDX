281080b4ba213528f5380ae3272e1577443ba564
Leobstere <jerrryzh163@gmail.com>
2025-11-07 01:04:50 -0500

    Trying to implement btBvhTriangleMeshShape on the chunks instead of individual blocks. The current idea is using a new class called PhysicsSystem and call it somewhere inbetween GameMeshBuilder and ChunkLoader to generate the collision detection. Then add the shape to a CollisionWorld.

--- core/src/main/java/io/github/testlibgdx/GameMeshBuilder.java
+++ core/src/main/java/io/github/testlibgdx/GameMeshBuilder.java
@@ -6,13 +6,12 @@ import Entity.World;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g3d.Material;
-import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.model.data.ModelData;
 import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.physics.bullet.collision.*;
 
 import java.util.ArrayList;
 import java.util.List;
